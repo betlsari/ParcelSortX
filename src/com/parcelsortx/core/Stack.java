67c32a75f5cdb061ad6efd310daae8db3cc4a120
Ece <akinecee3535@hotmail.com>
2025-05-16 19:01:58 +0300

    justt yorum satırı ekledim

--- src/com/parcelsortx/core/Stack.java
+++ src/com/parcelsortx/core/Stack.java
@@ -2,4 +2,9 @@ package com.parcelsortx.core;
 
 public class Stack {
 /* ECE */ 
+//return stack
+/* Bu yığın, yanlış yönlendirilen veya
+*  teslim edilemeyen kargoları geçici olarak depolamak için kullanılacak.*/ 
+	
+	
 }
