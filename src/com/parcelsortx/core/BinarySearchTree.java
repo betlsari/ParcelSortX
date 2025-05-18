24392e7b675bb60cf5ffbd489c1cbf9620237844
Ece <akinecee3535@hotmail.com>
2025-05-18 19:13:19 +0300

    coomit

--- src/com/parcelsortx/core/BinarySearchTree.java
+++ src/com/parcelsortx/core/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package com.parcelsortx.core;
 
+import com.parcelsortx.model.Parcel;
+
 public class BinarySearchTree {
 /* ECE */ 
 	private BinarySearchTreeNode root;
@@ -15,5 +17,40 @@ public class BinarySearchTree {
         this.highestParcelLoadCount = 0;
     }
 	
+	public void insertParcel(Parcel parcel) {
+        if (parcel == null) {
+            System.err.println("Hata: Eklenecek paket null olamaz.");
+            return;
+        }
+        root = insertParcelRecursive(root, parcel);
+    }
+	private BinarySearchTreeNode insertParcelRecursive(BinarySearchTreeNode current, Parcel parcel) {
+        if (current == null) {
+        	numOfNodes++; // Yeni bir şehir düğümü oluşturuldu
+            return new BinarySearchTreeNode(parcel.getDestinationCity(), parcel);
+        }
+
+        int compareResult = parcel.getDestinationCity().compareToIgnoreCase(current.cityName);
+
+        if (compareResult < 0) {
+            current.left = insertParcelRecursive(current.left, parcel);
+        } else if (compareResult > 0) {
+            current.right = insertParcelRecursive(current.right, parcel);
+        } else {
+            // Şehir düğümü zaten mevcut, paketi listeye ekle
+            current.parcelList.add(parcel);
+            // Paket eklendikten sonra en yüksek yüke sahip şehri kontrol et
+            if (current.getParcelCount() > highestParcelLoadCount) {
+                highestParcelLoadCount = current.getParcelCount();
+                cityWithHighestParcel = current.cityName;
+            }
+        }
+        return current;
+    }
 	
+	 public Queue<Parcel> getCityParcels(String city) {
+	        BSTNode node = findNode(root, city);
+	        return (node != null) ? node.parcelList : null;
+	    }
+
 }
